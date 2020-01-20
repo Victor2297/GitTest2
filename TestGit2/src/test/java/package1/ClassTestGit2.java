a405ec18e714efb45cb0a48b6b199623f219ae26
Victor <Victor@192.168.0.85>
2020-01-20 23:02:48 +0200

    Second Project

---
+++ TestGit2/src/test/java/package1/ClassTestGit2.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class ClassTestGit2 {
+	@Test
+	public void meth1() {
+		System.out.println("Salut");
+		System.out.println("Buna");
+		System.out.println("Noroc");
+	}
+
+}
